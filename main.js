cb6fcb4ca99dab004381b438c171f543799dad8e
Jamie Binns <jamie.m.binns@gmail.com>
2015-06-22 11:34:23 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,51 @@
+
+// Problem 1
+// var result = null;
+// for(var i=0; i<6; i++){
+// 	result = i * 100;
+// 	console.log(result);
+// }
+
+// Problem 2
+// var result = .5;
+// for(var i = 0; i < 7; i++){
+// 	result *= 2;
+// 	console.log(result);
+// }
+
+// Problem 3
+// var result = null;
+// for(var i = 1; i < 4; i++){
+// 	for(var x = 0; x < 3; x++){
+// 		console.log(i);
+// 	}
+// }
+
+// Problem 4
+// var result = null;
+// for(var i = 0; i < 6; i++){
+// 	result = i*2;
+// 	console.log(result);
+// }
+
+// Problem 5
+// var result = null;
+// for(var i = 0; i < 5; i++){
+// 	result+=3;
+// 	console.log(result);
+// }
+
+// Problem 6
+// var result = 10;
+// for(var i = 0; i < 10; i++){
+// 	result --;
+// 	console.log(result);
+// }
+
+// Problem 7
+var result = null;
+for(var i = 1; i < 4; i++){
+	for(var x = 0; x < 4; x++){
+		console.log(x);
+	}
+}
